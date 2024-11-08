9d4abb44d64917df7f7f9c3528c88a9cf8d09af5
edalzell <erin@silentz.co>
2024-11-07 17:52:31 -0800

    tidy

--- tests/Actions/ManagesCampaignsTest.php
+++ tests/Actions/ManagesCampaignsTest.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
- * Uncomment if you want to test against the real API
- *
-beforeEach(function () {
-    $this->mailcoach = new Mailcoach(
-        $_ENV['MAILCOACH_TOKEN'],
-        $_ENV['MAILCOACH_ENDPOINT'],
-    );
-});
-*/
-
 use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use Psr\Http\Message\RequestInterface;
